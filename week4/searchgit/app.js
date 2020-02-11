bd323d7649f8c705fe9edc1413e7439747fa5904
Ib <ismirza@pointpark.edu>
2020-02-11 17:50:35 -0500

    API

---
+++ week4/searchgit/app.js
@@ -0,0 +1,37 @@
+//init github
+const github = new GitHub();
+
+
+const searchUser = document.getElementById('searchUser');
+
+searchUser.addEventListener('keyup', (e) => {
+	const userText = e.target.value;
+
+	if(userText !== ''){
+		//make http call
+
+		github.getUser(userText)
+		.then(data => {
+			if(data.profile.message === 'Not Found'){
+				//show alert
+				document.getElementById('profile').innerHTML = `Name: Not Found`;
+			} else {
+				document.getElementById('profile').innerHTML = `
+				<div class="panel panel-default">
+				  <div class="panel-heading">
+				    <h3 class="panel-title">Name: ${data.profile.name}</h3>
+				  </div>
+				  <div class="panel-body">
+				    Repository: ${data.profile.public_repos} <br>
+				    Location: ${data.profile.location} <br>
+				    Bio: ${data.profile.bio}
+				  </div>
+				</div>`;
+			}
+		})
+	} else {
+		//Clear profile.
+
+		document.getElementById('profile').innerHTML = '';
+	}
+});
