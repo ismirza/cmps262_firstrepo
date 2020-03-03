7a63917fd17dcd5b150f1689d9f4e4bcdc82c4cf
Ib <ismirza@pointpark.edu>
2020-03-03 17:01:09 -0500

    edited the index page

--- 21sausage/index.js
+++ 21sausage/index.js
@@ -1,26 +1,26 @@
 //packages required
 var express = require('express');
-var exphbs  = require('express-handlebars');
+//var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser');
 const fs = require('fs');
-var mysql = require('mysql2');
-var session = require('express-session');
-var cookieParser = require('cookie-parser');
+//var mysql = require('mysql2');
+//var session = require('express-session');
+//var cookieParser = require('cookie-parser');
 var path = require('path');
-var validator = require('validator');
+//var validator = require('validator');
 
 // Initializing App
 var app = express();
 
 //Initializing Cookie Parser
-app.use(cookieParser());
+//app.use(cookieParser());
 
 // Setting up Session vars, will be evenutally moved to its own export
-app.use(session({
+/* app.use(session({
   secret: 'secret',
   resave: true,
   saveUninitialized: true,
-}));
+})); */
 
 //Setting Up bodparser inside of app
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,10 +28,13 @@ app.use(bodyParser.json());
 app.use(express.urlencoded());
 
 //setting up handlebars
-app.engine('handlebars', exphbs());
+//app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.locals.nav = GLOBALS.nav_items();
+//app.locals.nav = GLOBALS.nav_items();
+
+//Where we set the port for the app
+app.set('port', process.env.PORT || 3000);
 
 //ROUTING
 
